aee4236d90379a4ced15e227134052bddf542fd0
LucasAKuhn <lucasa.kuhn@hotmail.com>
2022-05-17 00:53:43 -0300

    exercicios

---
+++ src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+
+	public static void main(String[] args) {
+		double raio = 3.4;
+		final double pi = 3.14159;
+		//utilizando FINAL, o valor de pi fica uma CONSTANTE
+		
+		System.out.println(pi * raio * raio);
+	}
+}
