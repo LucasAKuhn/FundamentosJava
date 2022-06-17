bdf874e79ec627710626e540e9a00e2c4ebf3d9d
LucasAKuhn <lucasa.kuhn@hotmail.com>
2022-06-17 15:14:18 -0300

    s

--- src/classe/AreaCirc.java
+++ src/classe/AreaCirc.java
@@ -4,7 +4,7 @@ public class AreaCirc {
 
 	double raio;
 	final static double pi = 3.14;	//Valor de PI associado a classe, e não a instancia (static)
-	//final = valor do PI inalteravel
+	
 	
 	AreaCirc(double raioInicial) {
 		raio = raioInicial;
